936006e3fc8d7c38d612cb3fe42757fc99be0805
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2019-09-22 13:48:13 +0200

    Make pulse 'width' parameter adjust pulse width, not pause width

--- pygears_dsp/lib/pulse.py
+++ pygears_dsp/lib/pulse.py
@@ -1,6 +1,6 @@
 from pygears import gear
 from pygears.typing import Tuple, Uint
-from pygears.lib import cart, fmap, gt, rng
+from pygears.lib import cart, fmap, lt, rng
 
 TCfg = Tuple[{'period': Uint['w_period'], 'width': Uint['w_width']}]
 
@@ -8,6 +8,6 @@ TCfg = Tuple[{'period': Uint['w_period'], 'width': Uint['w_width']}]
 @gear
 def pulse(cfg: TCfg):
     """Generates pulse of variable length,
-    width is clk cycles for value 0"""
+    'width' is clk cycles for pulse width"""
     cnt = rng(0, cfg['period'], 1)
-    return cart(cnt, cfg['width']) | fmap(f=gt)
+    return cart(cnt, cfg['width']) | fmap(f=lt)
