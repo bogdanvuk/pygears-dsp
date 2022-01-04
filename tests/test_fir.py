2ec11e7446582207a5287c2a3634096dd4ad3490
Filip Dojcinovic <filip.dojcinovic@anari.ai>
2022-01-04 14:36:56 +0100

    adding pytest configuration and updating iir/fir tests, adding Makefile

--- tests/test_fir.py
+++ tests/test_fir.py
@@ -1,25 +1,162 @@
-import pytest
-from pygears_dsp.lib.fir import fir_direct, fir_transposed
-from pygears.lib import drv, collect, check
-from scipy.signal import firwin
-from pygears.typing import Fixp, Float
-from pygears.sim import sim
+import logging
+import random
+
 import numpy as np
+import pytest
+from scipy.signal.fir_filter_design import firwin
+
+from pygears import reg
+from pygears.lib import check, drv
+from pygears.sim import log, sim
+from pygears.sim.sim import cosim
+from pygears.typing import Fixp, Float
+from pygears_dsp.lib.fir import fir_direct, fir_transposed
+from tests.conftest import fixp_sat, random_choice_seq, random_seq, set_seed, sine_seq
+
+
+def fir_compare(x,y):
+    diff = abs(x-y)
+    dev = 1e-3
+    log.debug(f'fir_compare Result exp: {y},  got: {x}')
+    log.debug(f'fir_compare Result deviation {diff}')
+    if(diff > dev):
+        log.warning(f'fir_compare Result deviation error {diff}')
+    return diff < dev
+
+def fir_sim(impl,t_b,seq,do_cosim,target='build/fir'):
+    # get 'b' factors
+    b = firwin(8, [0.05, 0.95], width=0.05, pass_zero=False)
+    b_fixp = [t_b(i) for i in b]
+    
+    # get result
+    res = np.convolve(seq, b)
+    
+    # saturate the results value to filter output type if needed
+    for i,r in enumerate(res): 
+        res[i] = fixp_sat(t_b, r)
+    
+    # driving
+    drv(t=t_b, seq=seq) \
+        | impl(b=b_fixp) \
+        | Float \
+        | check(ref=res[:len(seq)], cmp=fir_compare)
+    
+    # optionally generate HDL code do co-simulation in verilator
+    if do_cosim:
+        cosim(f'{impl}', 'verilator', outdir=target, timeout=1000)
+    
+    # simulation start
+    sim(target, check_activity=False)
+    return res
+                
 
 
 @pytest.mark.parametrize('impl', [fir_direct, fir_transposed])
-def test_fir_direct(tmpdir, impl):
-    b = firwin(8, [0.05, 0.95], width=0.05, pass_zero=False)
+def test_fir_random(impl, seed, do_cosim):
+    # Set random seed
+    set_seed(seed)
+    
+    log.info(f'Running {__name__} impl: {impl}, seed: {seed}')
+    
+    t_b = Fixp[1,15]
+    
+    # genrate  random numbers in [-1,1)
+    seq = np.random.random(size=(100, )) * 2 - 1
+    log.debug(f'Generated sequence: {seq}')
+    
+    res = fir_sim(impl, t_b, seq, do_cosim=do_cosim)
+    
 
+@pytest.mark.parametrize('impl', [fir_direct, fir_transposed])
+def test_fir_random_type(impl, seed, do_cosim):
+    # Set random seed
+    set_seed(seed)
+    
+    log.info(f'{__name__} impl: {impl}, seed: {seed}')
+    
+    fixp_w = random.randint(16,32)
+    int_w = random.randint(1,3)
+
+    t_b = Fixp[int_w, fixp_w]
+    log.info(f'{__name__} FIR input type t_b: {t_b} min: {t_b.fmin}, max: {t_b.fmax}')
+
+    seq = []
+    # generate multiple constant sequences of certain size
+    for i in range(10):   
+        num = np.random.uniform(t_b.fmin, t_b.fmax)
+        for i in range(50):
+            seq.append(num)
+    # add a random sequence of certain size
+    seq.extend(random_seq(t_b, 100))
+    
+    # genrate  random numbers in [-1,1)
+    # seq = np.random.random(size=(100, )) * 2 - 1
+    log.debug(f'Generated sequence: {seq}')
+    
+    res = fir_sim(impl, t_b, seq, do_cosim=do_cosim)  
+    
+
+@pytest.mark.parametrize('impl', [fir_direct, fir_transposed])
+@pytest.mark.parametrize('fixp_w', range(16,33,4))
+@pytest.mark.parametrize('int_w', range(1,3))
+def test_fir_limits(fixp_w, int_w, impl, seed, do_cosim):
+    """[Drive filter with extreme values [min, 0 , max]
+    """
+    # Set random seed
+    set_seed(seed)
+    
+    log.info(f'Running {__name__} impl: {impl}, seed: {seed}')
+    
+    t_b = Fixp[int_w, fixp_w]
+    log.info(f'{__name__} FIR input type t_b: {t_b} min: {t_b.fmin}, max: {t_b.fmax}')
+    
+    extremes = [[0 for i in range(50)]]
+    extremes.append([t_b.fmin for i in range(50)])
+    extremes.append([t_b.fmax for i in range(50)])
+    seq = random_choice_seq(extremes,10)
+    
+       
+    # seq = np.random.triangular(-1,1,100)
+    log.debug(f'Generated sequence: {seq}')
+    
+    res = fir_sim(impl, t_b, seq, do_cosim=do_cosim)
+
+
+    
+@pytest.mark.parametrize('impl', [fir_direct, fir_transposed])
+@pytest.mark.parametrize('freq', [10_000, 100_000, 1_000_000])
+def test_fir_sine(freq, impl, seed, do_cosim):
+    """[Drive filter with sine signal at fs fs/2 and fs*2 
+    """
+    # Set random seed
+    set_seed(seed)
+    # set clock freq
+    reg['sim/clk_freq'] = freq
+    t = list(range(reg['sim/clk_freq']))[0:100]
+    fs = reg['sim/clk_freq']
+    f1 = freq/100    
+    
+    log.info(f'Running {__name__} impl: {impl}, seed: {seed}')
+    
     t_b = Fixp[1, 15]
-    b_fixp = [t_b(i) for i in b]
+    seq = [0 for i in range(10)]
+    seq.extend(sine_seq(f1, fs, 200, t_b))
+    seq.extend(sine_seq(f1, fs/2,100 , t_b))
+    seq.extend(sine_seq(f1, fs*2, 400, t_b))
+    seq.extend([0 for i in range(10)])
+ 
+    log.debug(f'Generated sequence: {seq}')
+    
+    res = fir_sim(impl, t_b, seq, do_cosim=do_cosim)   
 
-    x = np.random.random(size=(10, ))
-    res = np.convolve(x, b)
+    
+# reg['debug/trace'] = ['*']
+# reg['debug/webviewer'] = True
+# reg['logger/sim/error'] = 'debug'  # on error open cmdline debugger
+# reg['logger/sim/error'] = 'continue'  # on error continue
+# reg['logger/sim/level'] = logging.DEBUG
 
-    drv(t=t_b, seq=x) \
-        | impl(b=b_fixp) \
-        | Float \
-        | check(ref=res[:len(x)], cmp=lambda x, y: abs(x-y) < 1e-3)
-
-    sim(tmpdir, check_activity=False)
+## HINT : for debugging individual tests
+# test_fir_random(fir_direct, 12, do_cosim=True)
+# test_fir_limits(fir_transposed, 1, do_cosim=True)
+# test_fir_sine(1_000_000,fir_direct, 12, do_cosim=True)
--- tests/test_iir.py
+++ tests/test_iir.py
@@ -1,20 +1,78 @@
-from scipy import signal
-from pygears_dsp.lib.iir import iir_df1dsos, iir_df2tsos
-from pygears.sim import sim
-from pygears_control.lib import scope
-from pygears import config
+import logging
+import random
 from math import pi, sin
-import pytest
-from pygears.lib import check, drv
-from pygears.typing import Fixp, Float
-from pygears.sim import sim
 
+import pytest
+from scipy import signal
+
+# from pygears_control.lib import scope
+from pygears import reg
+from pygears.lib import check, drv
+from pygears.sim import log, sim
+from pygears.sim.sim import cosim
+from pygears.typing import Fixp, Float
+from pygears_dsp.lib.iir import iir_df1dsos, iir_df2tsos
+from tests.conftest import constant_seq, fixp_sat, random_choice_seq, random_seq, set_seed, sine_seq
+
+
+def iir_compare(x,y):
+    diff = abs(x-y)
+    dev = 1e-3
+    log.debug(f'{__name__}, exp:{y}, got:{x}')
+    log.debug(f'{__name__} deviation:{diff}')
+    if(diff>dev):
+        log.warning(f'{__name__} deviation error {diff}')
+    return diff < dev
+
+def iir_sim(impl,t_coef, t_in, t_out,seq,target='build/fir',do_cosim=False):
+    # create SOS referent filter and factors
+    sos = signal.butter(
+    N=5, Wn=30000 / 100000, btype='lowpass', analog=False, output='sos')
+
+    # get a,b coefficient from the created filter
+    a, b = [], []
+    for s in sos:
+        b.append(list(s[0:3]))
+        a.append(list(s[3:]))
+        
+    # convert coefficient to wanted Fixp type
+    b = [[t_coef(coef) for coef in section] for section in b]
+    a = [[t_coef(coef) for coef in section] for section in a]
+    
+    log.info(f'Generated B coeff: {b}')
+    log.info(f'Generated A coeff: {a}')
+    
+    gain = [t_in(1)] * len(b)
+    ref = signal.sosfilt(sos, seq)
+    
+    # fp_ref = [float(r) for r in ref]
+    # saturate the results value to filter output type if needed
+    for i,r in enumerate(ref): 
+        ref[i] = fixp_sat(t_out, float(r))
+    
+    log.info(f'Generated sequence: {seq}')
+    log.info(f'Refferenc result: {ref}')
+    drv(t=t_in, seq=seq) \
+    | impl(a=a, b=b, gain=gain, ogain=t_in(1)) \
+    | Float \
+    | check(ref=ref[:len(seq)], cmp=iir_compare)
+    
+    # optionally generate HDL code do co-simulation in verilator
+    if do_cosim:
+        cosim(f'{impl}', 'verilator', outdir=target, timeout=1000)
+        
+    sim(target, check_activity=False)
+    return ref
+    
 
 @pytest.mark.parametrize('impl', [iir_df1dsos, iir_df2tsos])
-def test_iir_direct(tmpdir, impl):
-    config['sim/clk_freq'] = 100000
-    t = list(range(config['sim/clk_freq']))[0:100]
-    fs = config['sim/clk_freq']
+def test_iir_direct(tmpdir, impl, seed, do_cosim):
+    reg['sim/rand_seed'] = seed
+    random.seed(reg['sim/rand_seed'])
+    log.info(f'Running test_fir_direct tmpdir: {tmpdir}, impl: {impl}, seed: {seed}')
+    reg['sim/clk_freq'] = 100000
+    t = list(range(reg['sim/clk_freq']))[0:100]
+    fs = reg['sim/clk_freq']
     f1 = 1000
     f2 = 70000
 
@@ -31,21 +89,115 @@ def test_iir_direct(tmpdir, impl):
         b.append(list(s[0:3]))
         a.append(list(s[3:]))
 
-    t_coef = Fixp[2, 32]
+    t_coef = Fixp[5, 32]
+    
 
     b = [[t_coef(coef) for coef in section] for section in b]
     a = [[t_coef(coef) for coef in section] for section in a]
 
-    gain = [Fixp[1, 23](1)] * len(b)
+    gain = [Fixp[2, 23](1)] * len(b)
     ref = signal.sosfilt(sos, seq)
     fp_ref = [float(r) for r in ref]
 
+    
+    log.info(f'Generated sequence: {seq}')
     drv(t=Fixp[5, 24], seq=seq) \
-        | impl(a=a,b=b, gain=gain, ogain=1) \
-        | Float \
-        | check(ref=fp_ref[:len(seq)], cmp=lambda x, y: abs(x-y) < 1e-3)
+    | impl(a=a, b=b, gain=gain, ogain=Fixp[5, 24](1)) \
+    | Float \
+    | check(ref=fp_ref[:len(seq)], cmp=lambda x, y: abs(x - y) < 1e-3)
+    
+    if do_cosim:
+        cosim(f'{impl}', 'verilator', outdir=tmpdir, timeout=1000)
 
     sim(tmpdir, check_activity=False)
 
+@pytest.mark.parametrize('impl', [iir_df1dsos, iir_df2tsos])
+def test_iir_random(impl, seed, do_cosim):
+    log.info(f'Running test_iir_random seed: {seed}')
+    set_seed(seed)
+    
+    ftype = Fixp[5,32]
 
-# test_iir_direct('build', impl=iir_df1dsos)
+    seq = constant_seq(ftype, 10, 0)
+    seq.extend(random_seq(ftype, 100))
+    seq.extend(constant_seq(ftype, 10, 0))
+    
+    iir_sim(impl, ftype, ftype, ftype, seq, do_cosim=do_cosim)
+    
+@pytest.mark.parametrize('impl', [iir_df1dsos, iir_df2tsos])
+def test_iir_random_type(impl, seed, do_cosim):
+    log.info(f'Running test_iir_random, seed: {seed}')
+    set_seed(seed)
+    
+    # minimum supported precision Fixp[3,19]
+    int_w = random.randint(3,7)
+    fixp_w = random.randint(int_w+19,int_w+32)
+    ftype = Fixp[int_w,fixp_w]
+    
+    log.info(f'{__name__} FIR input type t_b: {ftype} min: {ftype.fmin}, max: {ftype.fmax}')
+    
+    seq = constant_seq(ftype, 10, 0)
+    seq.extend(random_seq(ftype, 100))
+    seq.extend(constant_seq(ftype, 10, 0))
+    
+    iir_sim(impl, ftype, ftype, ftype, seq, do_cosim=do_cosim)
+ 
+# HINT -> IIR implementation errors when testing extreme inputs
+# errors are due to different precision used than reference results
+@pytest.mark.parametrize('impl', [iir_df1dsos, iir_df2tsos])
+@pytest.mark.parametrize('fixp_w', range(25,33,4))
+@pytest.mark.parametrize('int_w', range(3,7))
+def test_iir_limits(fixp_w, int_w, impl, seed, do_cosim):   
+    """[Drive filter with extreme values [min, 0 , max]
+    """
+    log.info(f'Running test_iir_limits, seed: {seed}')
+    set_seed(seed)
+    factor = 0.5 # supported factor
+    
+    ftype = Fixp[int_w, fixp_w]
+    print(f'max possible value {ftype.fmax}')
+    extremes = [[0 for i in range(50)]]
+    extremes.append([ftype.fmin*factor for i in range(10)])
+    seq = random_choice_seq(extremes,10)
+    extremes.append([ftype.fmax*factor for i in range(10)])
+    seq = random_choice_seq(extremes,10)
+    iir_sim(impl, ftype, ftype, ftype, seq, do_cosim=do_cosim)
+    
+    
+@pytest.mark.parametrize('impl', [iir_df1dsos, iir_df2tsos])
+@pytest.mark.parametrize('freq', [10_000, 100_000, 1_000_000])
+def test_fir_sine(freq, impl, seed, do_cosim):
+    """[Drive filter with sine signal at fs fs/2 and fs*2 
+    """
+    # Set random seed
+    set_seed(seed)
+    # set clock freq
+    reg['sim/clk_freq'] = freq
+    t = list(range(reg['sim/clk_freq']))[0:100]
+    fs = reg['sim/clk_freq']
+    f1 = freq/100 
+    log.info(f'Running {__name__} impl: {impl}, seed: {seed}, fs:{fs},f1:{f1}')    
+    
+    ftype = Fixp[5,32]
+    seq = [0 for i in range(10)]
+    seq.extend(sine_seq(f1, fs, 200, ftype))
+    seq.extend([0 for i in range(100)])    
+    seq.extend(sine_seq(f1, fs/2,100 , ftype))
+    seq.extend([0 for i in range(100)])    
+    seq.extend(sine_seq(f1, fs*2, 400, ftype))
+    seq.extend([0 for i in range(10)])    
+    res = iir_sim(impl, ftype, ftype, ftype, seq, do_cosim=do_cosim) 
+    
+# reg['logger/sim/error'] = 'debug'  # on error open cmdline debugger
+# reg['logger/sim/error'] = 'continue'  # on error continue
+# reg['logger/sim/level'] = logging.DEBUG
+# reg['debug/trace'] = ['*']
+# reg['debug/webviewer'] = True
+
+## HINT : for debugging individual tests
+# test_iir_direct('build', iir_df2tsos, 2,do_cosim=False)
+# test_iir_random(iir_df2tsos, 1,do_cosim=False)
+# test_iir_random_type(iir_df2tsos, 1,do_cosim=False)
+# test_iir_limits(46,3,iir_df2tsos,1,do_cosim=False)
+# test_fir_sine(100_000,iir_df2tsos, 12,do_cosim=False)
+
